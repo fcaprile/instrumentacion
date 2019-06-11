eb6f0ea53b99e07b8513c91e33a31bfc1821d933
fcaprile <36137269+fcaprile@users.noreply.github.com>
2019-06-11 19:57:04 -0300

    clase 12/06

---
+++ lantz_servo.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 05:10:00 2019
+
+@author: Publico
+"""
+import lantz
+import time
+from lantz.ino import INODriver, QuantityFeat, BoolFeat
+import cv2
+
+#cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+class Servo(INODriver):       
+    angulo = QuantityFeat('Angulo', setter=True)
+    enable = BoolFeat('ENABLE')
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
+
+
+
+if __name__ == '__main__':
+    with Servo.via_packfile('Servo.pack.yaml') as dev:
+        print('Hola, maestros de la programacion y el universo')
+        for i in range(150):
+            dev.angulo=i*1.0
+        for i in range(150):
+            dev.angulo=150-i
