a51e79e0cd355c8fb1de598f1399b47900e8ccf0
fcaprile <fcaprile@gmail.com>
2019-06-24 23:50:19 -0300

    magia

--- encontrar_centro_circulo.py
+++ encontrar_centro_circulo.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 
 def chi(Xc,args):
     xc,yc,R=Xc
-    x1,y1,x2,y2,x3,y3,x4,y4=args
+    x1,y1,x2,y2,x3,y3=args
     return abs(3*R**2-(x1-xc)**2-(y1-yc)**2-(x2-xc)**2-(y2-yc)**2-(x3-xc)**2-(y3-yc)**2)#-(x4-xc)**2-(y4-yc)**2
 
 #def chi(Xc,args):
@@ -19,7 +19,7 @@ def chi(Xc,args):
 #    x=args
 #    return abs(a-x)
 #
-puntos=np.array([10,10,-10,10,0,-14.14,14.14,0])
+puntos=np.array([10,10,-10,10,0,-14.14])
 #puntos=2
 x0=np.array([0,0,14.14])
 #x0=np.array([0])
--- tpfinal/tpfinal_bn.py
+++ tpfinal/tpfinal_bn.py
@@ -18,6 +18,7 @@ from lantz.ino import INODriver, QuantityFeat, BoolFeat
 import cv2
 import numpy as np
 import math
+from scipy.optimize import minimize as minim
 #cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
@@ -51,40 +52,79 @@ def calcular_centro(gray):
     posy=480-posy
     return posx,posy,matriz
 
+def medir_centro(dev,angulo):
+    dev.angulo=angulo
+    rval, frame = vc.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    posx,posy,matriz=calcular_centro(gray)
+    return posx,posy
+
+def chi(Xc,args):
+    xc,yc,R=Xc
+    x1,y1,x2,y2,x3,y3=args
+    return abs(3*R**2-(x1-xc)**2-(y1-yc)**2-(x2-xc)**2-(y2-yc)**2-(x3-xc)**2-(y3-yc)**2)#-(x4-xc)**2-(y4-yc)**2
+
+
 angulo_a_mandar=0
-posxc=0
-posyc=
-inicio=0
+xc=0
+yc=0
+r=0
+inicio=True
+
 if __name__ == '__main__':
     with Servo.via_packfile('Servo.pack.yaml') as dev:        
         print('Hola')            
         while rval:
-            if inicio==0:
-                dev.angulo=0
-                rval, frame = vc.read()
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                posx1,posy1,matriz=calcular_centro(gray)
-                dev.angulo=30
-                rval, frame = vc.read()
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                posx2,posy2,matriz=calcular_centro(gray)
-#                dev.angulo=120
-#                rval, frame = vc.read()
-#                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#                posx3,posy3,matriz=calcular_centro(gray)
-
-            d=posy1-posy2/-0.5
+            if inicio==True:
+                posx1,posy1=medir_centro(dev,0)
+                posx2,posy2=medir_centro(dev,60)
+                posx3,posy3=medir_centro(dev,120)
+                x0=np.array([320,240,50])
+                puntos=np.array([posx1,posy1,posx2,posy2,posx3,posy3])
+                res=minim(chi,x0,puntos)
+                xc,yc,r=res.x
+                inicio=False
             
-
             rval, frame = vc.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            posx,posy=calcular_centro(gray)
+            posx,posy,matriz=calcular_centro(gray)
             
             if posx-len(gray[0,:])/2==0:
                 tita=1
             else:
+                tita=math.atan2((posy-yc),(posx-xc))*180/np.pi
+            print('El angulo es:',tita)            
+            cv2.imshow("la magia", matriz)            
+            key = cv2.waitKey(20)
+            if key == 27: # exit on ESC
+                break
+            if key == 32: # apretando espacio renueva medicion del centro
+                inicio=True
+            angulo0=0 #por si hace falta agregarlo
+            angulo_a_mandar+=angulo0+tita*0.2
+            if abs(angulo_a_mandar)>90:
+                angulo_a_mandar=0
+            print('Estamos mandando:',angulo_a_mandar+90)
+            dev.angulo=angulo_a_mandar+90
+        cv2.destroyWindow("preview")
+
+'''
+                dev.angulo=0
+                rval, frame = vc.read()
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                posx1,posy1,matriz=calcular_centro(gray)
+                dev.angulo=60
+                rval, frame = vc.read()
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                posx2,posy2,matriz=calcular_centro(gray)
+                dev.angulo=120
+                rval, frame = vc.read()
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                posx3,posy3,matriz=calcular_centro(gray)
+'''
+
+'''
 #                tita_rad=math.atan((posy-240)/(posx-320))
-                tita=math.atan2((posy-240),(posx-320))*180/np.pi
 #            if posy<240 and posy<320:
 #                tita=tita_rad/np.pi*180
 #            if posy>240 and posy<320:
@@ -95,17 +135,4 @@ if __name__ == '__main__':
 #                tita=-tita_rad/np.pi*180
             
 #            print(posx,posy,tita)        
-            print('El angulo es:',tita)            
-            cv2.imshow("la magia", matriz)            
-            key = cv2.waitKey(20)
-            if key == 27: # exit on ESC
-                break
-            angulo0=0
-            angulo_a_mandar+=angulo0+tita*0.2
-            if abs(angulo_a_mandar)>90:
-                angulo_a_mandar=0
-            print('Estamos mandando:',angulo_a_mandar+90)
-            dev.angulo=angulo_a_mandar+90
-        cv2.destroyWindow("preview")
-
-
+'''
